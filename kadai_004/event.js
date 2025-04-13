b5ee39b056d93dc65380a47621c93d907712c31b
Liberenren <164756775+Liberenren@users.noreply.github.com>
2025-04-14 01:18:32 +0900

    Update event.js

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,10 +1,10 @@
 $(function() {
     // ボタンがクリックされた場合
-    $(document).on('load',function(){
+    $('windows').on('load',function(){
         console.log('loadイベントが発生しました');
        });
        
-    $('.box').on('scroll',function(){
+    $('windows').on('scroll',function(){
         console.log('scrollイベントが発生しました');
     });
    });
